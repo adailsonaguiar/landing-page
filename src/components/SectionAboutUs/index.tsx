1a26c8d3a5b59e0a495c8667fcbf3f37963bd56b
unknown <adailsonacj@live.com>
2020-08-28 00:06:11 -0300

    fix

--- src/components/SectionAboutUs/index.tsx
+++ src/components/SectionAboutUs/index.tsx
@@ -10,7 +10,6 @@ import * as S from './styles'
 const SectionAboutUs = ({ title, authors }: SectionAboutUsProp) => (
   <Container>
     <Heading reverseColor>{title}</Heading>
-    {console.log(authors)}
     <S.Content>
       {authors.map((profile) => (
         <ProfileCard
